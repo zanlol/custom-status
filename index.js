981ec1069285f52104d54242765a0cf55afbe2c5
zanlol <120652123+zanlol@users.noreply.github.com>
2025-05-16 11:42:37 +0800

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = [".gg/QNH8JM5YH8 JOIN NOW, runned by jal, ang cute talaga ni mizu"];
+const statusMessages = [".gg/QNH8JM5YH8 JOIN NOW❗","runned by jal","ang cute talaga ni mizu"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
